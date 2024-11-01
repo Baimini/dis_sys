e2c69854b6080ae4cd4be977d0ebffc88c7897b0
Baimini <98502673+Baimini@users.noreply.github.com>
2024-11-01 15:38:56 +0800

    All Finished

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -8,6 +8,12 @@ const Layout = () => {
                     <li>
                         <Link to = "/">Home</Link>
                     </li>
+                    <li>
+                        <Link to = "/get">Get All User</Link>
+                    </li>
+                    <li>
+                        <Link to = "/post">Post User</Link>
+                    </li>
                 </ul>
             </nav>
             <Outlet/>
